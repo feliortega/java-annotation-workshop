10fd68a91f298a4b6f14462517ab40b113008005
Felipe Ortega <felipe.ortega@mercadolibre.cl>
2022-03-15 13:02:57 -0300

    feat: annotation processor

--- app/src/main/java/com/github/feliortega/App.java
+++ app/src/main/java/com/github/feliortega/App.java
@@ -2,6 +2,6 @@ package com.github.feliortega;
 
 public class App {
   public static void main(String[] cmdArgs) {
-    System.out.println("hello world");
+    System.out.println("hello world!2");
   }
 }
---
+++ lib/src/main/java/com/github/feliortega/processor/AnnotationProcessor.java
@@ -0,0 +1,28 @@
+package com.github.feliortega.processor;
+
+import java.util.Set;
+import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.RoundEnvironment;
+import javax.lang.model.SourceVersion;
+import javax.lang.model.element.TypeElement;
+import javax.tools.Diagnostic;
+
+public class AnnotationProcessor extends AbstractProcessor {
+  @Override
+  public boolean process(Set<? extends TypeElement> set, RoundEnvironment roundEnvironment) {
+    set.forEach(System.out::println);
+
+    processingEnv.getMessager().printMessage(Diagnostic.Kind.ERROR, "run");
+    return false;
+  }
+
+  @Override
+  public SourceVersion getSupportedSourceVersion() {
+    return SourceVersion.RELEASE_11;
+  }
+
+  @Override
+  public Set<String> getSupportedAnnotationTypes() {
+    return Set.of("*");
+  }
+}
